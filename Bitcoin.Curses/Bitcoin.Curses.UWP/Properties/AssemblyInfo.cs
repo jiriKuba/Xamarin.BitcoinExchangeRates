abc56b95b08293beb569590ad272137f3200049c
Jiří Kuba <kuba.jirka90@gmail.com>
2017-01-29 20:20:05 +0100

    Add accurate version

--- Bitcoin.Curses/Bitcoin.Curses.UWP/Properties/AssemblyInfo.cs
+++ Bitcoin.Curses/Bitcoin.Curses.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bitcoin exchange rates")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Bitcoin Exchange Rate")]
+[assembly: AssemblyDescription("Application will get actual bitcoin exchange rates.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jiri Kuba")]
 [assembly: AssemblyProduct("Bitcoin.Curses.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Jiri Kuba 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
 [assembly: ComVisible(false)]
--- Bitcoin.Curses/Bitcoin.Curses/Properties/AssemblyInfo.cs
+++ Bitcoin.Curses/Bitcoin.Curses/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bitcoin.Curses")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Application will get actual bitcoin exchange rates.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jiri Kuba")]
 [assembly: AssemblyProduct("Bitcoin.Curses")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Jiri Kuba 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.1.0")]
+[assembly: AssemblyFileVersion("0.9.1.0")]
