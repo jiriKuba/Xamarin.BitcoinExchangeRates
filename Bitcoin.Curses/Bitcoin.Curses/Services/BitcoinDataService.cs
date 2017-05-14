85947adfb9259095f3b2b482d3e15aeb13853161
Jiří Kuba <kuba.jirka90@gmail.com>
2017-05-14 12:50:15 +0200

    Updated android icons

--- Bitcoin.Curses/Bitcoin.Curses/Services/BitcoinDataService.cs
+++ Bitcoin.Curses/Bitcoin.Curses/Services/BitcoinDataService.cs
@@ -36,8 +36,6 @@ namespace Bitcoin.Curses.Services
         {
             try
             {
-                //await Task.Delay(TimeSpan.FromSeconds(3));
-
                 var rawBitcoinExchangeRates = await _dataProvideService.GetBitcoinJSONData();
                 var bitcoinRateValues = JsonConvert.DeserializeObject<Dictionary<string, BitcoinExchangeRate>>(rawBitcoinExchangeRates);
 
