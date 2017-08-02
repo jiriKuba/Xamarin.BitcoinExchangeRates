63e58f1662b8bf4c68ba1d892bfea4e7cfff5278
Jiří Kuba <kuba.jirka90@gmail.com>
2017-08-02 19:56:19 +0200

    Added difference procenta + label update

--- Bitcoin.Curses/Bitcoin.Curses/ViewModel/ExchangeRateViewModel.cs
+++ Bitcoin.Curses/Bitcoin.Curses/ViewModel/ExchangeRateViewModel.cs
@@ -59,6 +59,32 @@ namespace Bitcoin.Curses.ViewModel
             }
         }
 
+        public decimal MarketPriceDifferencePercentage
+        {
+            get
+            {
+                if (_exchangeRate != null)
+                {
+                    if (_exchangeRate.YesterdayRate == 0)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        var firstVal = _exchangeRate.RecentMarketPrice > _exchangeRate.YesterdayRate ? _exchangeRate.RecentMarketPrice : _exchangeRate.YesterdayRate;
+                        var secVal = firstVal == _exchangeRate.RecentMarketPrice ? _exchangeRate.YesterdayRate : _exchangeRate.RecentMarketPrice;
+                        if (secVal == 0)
+                        {
+                            return 0;
+                        }
+
+                        return (firstVal * 100 / secVal) - 100;
+                    }
+                }
+                else return 0;
+            }
+        }
+
         public string MarketPriceDifferenceLabel
         {
             get
@@ -70,23 +96,23 @@ namespace Bitcoin.Curses.ViewModel
                         var rateValue = MarketPriceDifference.Value;
                         if (rateValue >= 0)
                         {
-                            return "+ " + rateValue.ToString("N2");
+                            return "+ " + rateValue.ToString("N2") + $"({MarketPriceDifferencePercentage.ToString("N2")}%)";
                         }
                         else if (rateValue < 0)
                         {
-                            return "- " + Math.Abs(rateValue).ToString("N2");
+                            return "- " + Math.Abs(rateValue).ToString("N2") + $"({MarketPriceDifferencePercentage.ToString("N2")}%)";
                         }
                         else
                         {
-                            return "+ " + decimal.Zero.ToString("N2");
+                            return "+ " + decimal.Zero.ToString("N2") + "(0%)";
                         }
                     }
                     else
                     {
-                        return "+ " + decimal.Zero.ToString("N2");
+                        return "+ " + decimal.Zero.ToString("N2") + "(0%)";
                     }
                 }
-                else return "+ " + decimal.Zero.ToString("N2");
+                else return "+ " + decimal.Zero.ToString("N2") + "(0%)";
             }
         }
 
